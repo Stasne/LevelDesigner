7a2e2d56e1ca4d2a5b65b9e5b6bacc0b041a421a
Stas Ne <sstusne@yandex.ru>
2022-05-23 03:17:12 +0400

    Resource manager + export

--- ResourceManager.py
+++ ResourceManager.py
@@ -1,32 +1,116 @@
 import sys
+import json
+import resourcer_design
+from os import getcwd
+from os.path import exists
 from PyQt5.QtWidgets import *#QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QHeaderView, QLabel
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-class Resourcer(QWidget):
+
+class Resourcer(QWidget, resourcer_design.Ui_Form):
 	def __init__(self):
 		super().__init__()
-		self.resize(600,400)
-		self.layout = QHBoxLayout()
-		item = QLabel("Dsdsd")
-		self.itemName   = QLineEdit(item)
-		self.itemName.setReadOnly(True)
+		self.setupUi(self)
+		self.resource_img = str("")
+		self.resources_dir = "Resources"
+		self.resource_ext = ".resource"
+		self.resource_dest_path = ""
+		self.pbAddImg.clicked.connect(self.onAddImageButton)
+		self.pbSave.clicked.connect(self.onSave)
+		self.leCode.textChanged.connect(lambda: self.checkInput())
+		self.pbSave.setEnabled(False)
 
-		self.itemCount  = QLineEdit()
-		self.itemCount.setText("0")
-		self.itemCount.setMaximumWidth(40)
+	def updateImage(self):
+		pixmap = QPixmap(self.resource_img)
+		self.lblDisplay.setPixmap(pixmap)
 
-		self.decrease   = QPushButton("-")
-		self.increase   = QPushButton("+")
+	def onAddImageButton(self):
+		dlg = QFileDialog()
+		dlg.setFileMode(QFileDialog.ExistingFile)
+		dlg.setNameFilters(["Text files (*.txt)", "Images (*.png *.jpg)"])
+		dlg.selectNameFilter("Images (*.png *.jpg)")
+		if dlg.exec_():
+			self.resource_img = dlg.selectedFiles()[0]
+			self.lblImgPath.setText(self.resource_img)
+			self.updateImage()
 
-		self.layout.addWidget(self.itemName)
-		self.layout.addWidget(self.itemCount)
-		self.layout.addWidget(self.decrease)
-		self.layout.addWidget(self.increase)
+	def onSave(self):
+		if (not self.checkInput()):
+			QMessageBox.critical(self, 'CRITICAL ERROR', 'Something wrong with provided data')
+			return False
 
-		self.setLayout(self.layout)
+		file_exists = exists(self.resource_dest_path)
+		if (file_exists):
+			res = QMessageBox.question(self,'File exists',"File '" + self.resource_dest_path +"' exists, rewrite?")
+			if (res != QMessageBox.Yes):
+				return False
+		self.resource_code = self.leCode.text()
+		self.resource_comment = self.teComment.toPlainText()
+		self.exportResource()
 
+	def checkInput(self):
+		if (not self.resource_img):
+			return False
+		if (not self.leCode.text()):
+			return False
+		resource_code = self.leCode.text()
+		if (not resource_code):
+			return False
+		self.resource_dest_path = getcwd() + '/'+ self.resources_dir +'/' + resource_code + self.resource_ext
+		self.pbSave.setEnabled(True)
+		return True
+
+	def exportResource(self):
+		print ("Export resource")
+		self.toJson()
+		QMessageBox.information(self, 'Success', 'Success \nResource saved to:' + self.resource_dest_path)
+
+	def editor(self):
+		print("Edit resource")
+		dlg = QFileDialog()
+		dlg.setFileMode(QFileDialog.ExistingFile)
+		filters = "Resources (*" + self.resource_ext + ")"
+		dlg.setNameFilters([filters])
+		dlg.selectNameFilter(filters)
+		if (not dlg.exec_()):
+			return False
 		
+		self.resource_dest_path = dlg.selectedFiles()[0]
+		self.importResource(self.resource_dest_path)
+		self.leCode.setEnabled(False)
+		self.show()
+
+	def toJson(self):
+		if (not self.checkInput()):
+			QMessageBox.critical(self, 'CRITICAL ERROR', 'Something wrong with provided data')
+
+		properties = dict()
+		meAsDict = dict()
+		dictionary ={
+			"type" : "resource",
+		    "code" : self.resource_code,
+		    "img" : self.resource_img,
+		    "comment" : self.resource_comment
+			}
+		# Serializing json 
+		# return json_object = json.dumps(dictionary, indent = 4)
+
+		json_object = json.dumps(dictionary, indent = 4)
+		# Writing to sample.json
+		# out_file = self.resource_code + self.resource_ext
+		with open(self.resource_dest_path, "w") as outfile:
+			outfile.write(json_object)
+
+	def importResource(self, resource_path):
+		print("Import resource: " + resource_path)
+
+	def closeEvent(self, event):
+		if (self.checkInput()):
+			res = QMessageBox.question(self,'R u sure?',"Cancel changes?")
+			if (res != QMessageBox.Yes):
+				return event.ignore()
+
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -2,48 +2,52 @@ import sys
 import design
 import resources
 import ResourceManager
+import copy
 from PyQt5.QtWidgets import *#QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QHeaderView, QLabel
 from PyQt5.QtGui import QPixmap, QIcon, QBrush
 from PyQt5.QtCore import *
+
 ItemStyle_selected = "border: 2px solid white;"
 ItemStyle_normal = "border: 0px;"
+
 class LevelItem(QLabel):
     def __init__(self):
         super().__init__()
         self.setMouseTracking(True)
 
-    def mouseEvent(self, event):
-        print("mouse")
-        return super(LevelItem, self).mouseEvent(event)
-    def enterEvent(self, event):
-        # print ("Mouse Entered")
-        # setting up border
-        self.setStyleSheet(ItemStyle_selected)
-        return super(LevelItem, self).enterEvent(event)
+    # def enterEvent(self, event):
+    #     self.setStyleSheet(ItemStyle_selected)
+    #     print("enter event")
+    #     return super(LevelItem, self).enterEvent(event)
 
-    def leaveEvent(self, event):      
-        # print ("Mouse Left")
-        self.setStyleSheet(ItemStyle_normal)
-        return super(LevelItem, self).enterEvent(event)
+    # def leaveEvent(self, event):      
+    #     self.setStyleSheet(ItemStyle_normal)
+    #     print("leave event")
+    #     return super(LevelItem, self).enterEvent(event)
 
-    def mouseReleaseEvent(self, event):  
-        posMouse =  event.pos()
-        if self.rect().contains(posMouse):
-            print ("Under Mouse")
-        return super(LevelItem, self).mouseReleaseEvent(event)
+    # def mouseReleaseEvent(self, event):  
+    #     posMouse =  event.pos()
+    #     print("mouse release event")
+    #     if self.rect().contains(posMouse):
+    #         print ("Under Mouse")
+    #     return super(LevelItem, self).mouseReleaseEvent(event)
 
 class MainWindow(QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setMouseTracking(True)
         self.setupUi(self)
         self.setWindowTitle('Level Designer')
-        self.createEditor()
-        # СДЕЛАТЬ СВЯЗКУ НА РЕСУР МАНАГЕР  И ЕГО СДЕЛАТЬ
-        self.actionNew_Resource.triggered.connect(lambda: print("RESS"))
-        
-    def createEditor(self):
-        self.twMain.setColumnCount(30)
-        self.twMain.setRowCount(15)
+        self.createEditor(15, 30)
+        self.resourcer = ResourceManager.Resourcer()
+        self.actionCreate_resource.triggered.connect(lambda: self.resourcer.show())
+        self.actionEdit_resource.triggered.connect(lambda: self.resourcer.editor())
+        self.actionExitBut.triggered.connect(lambda: QApplication.closeAllWindows() )
+        self.brush = self.makeLevelItem(":/Resources/WL.png")
+
+    def createEditor(self, rows, cols):
+        self.twMain.setColumnCount(cols)
+        self.twMain.setRowCount(rows)
         for j in range(self.twMain.rowCount()):
             for k in range(self.twMain.columnCount()):
                 item = self.makeLevelItem(":/Resources/__.png")
@@ -56,6 +60,10 @@ class MainWindow(QMainWindow, design.Ui_MainWindow):
 
         #signals
         self.twMain.itemSelectionChanged.connect(self.onSelection)
+        # self.twMain.itemEntered.connect(self.onSelection)
+        # self.twMain.cellEntered.connect(self.onCellEntered)
+        # self.twMain.cellActivated.connect(self.onCellActivated)
+        self.twMain.cellPressed.connect(self.onCellPressed)
 
     def makeLevelItem(self, image):
         lbl = LevelItem()
@@ -64,13 +72,30 @@ class MainWindow(QMainWindow, design.Ui_MainWindow):
         lbl.setPixmap(pixmap)
         return lbl
 
-    def onSelection(self):
-        print(len(self.twMain.selectedItems()))
-        # self.setStyleSheet(ItemStyle_selected)
+    def onCellEntered(self, row, col):
+        item = self.twMain.item(row, col)
+        item = type(self.brush)()
+        self.twMain.setCellWidget(row,col, item)
+        # print ("table: Cell entered row: " + str(row) + " col: " + str(col))
     
+    def onCellActivated(self, row, col): #doubleclick
+        print ("table: Cell activated row: " + str(row) + " col: " + str(col))    
+
+    def onCellPressed (self, row, col):
+        print ("table: Cell presed row: " + str(row) + " col: " + str(col))        
+    def onSelection(self):
+        print("table: selection changed")
+        # print(len(self.twMain.selectedItems()))
+        print(len(self.twMain.selectedRanges()))
+
+
+    
+    def closeEvent(self, event):
+        QApplication.closeAllWindows() 
 
 
 def main():
+    
     app = QApplication(sys.argv)
     view = MainWindow()
     view.show()
